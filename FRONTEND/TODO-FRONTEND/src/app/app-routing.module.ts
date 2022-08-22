6a6f2fb992fcf6ba1421f2b6fb6b5965b8abc3f5
BlokOfWood <strikerelite@outlook.hu>
2022-08-22 09:24:24 +0200

    defined routing paths

--- FRONTEND/TODO-FRONTEND/src/app/app-routing.module.ts
+++ FRONTEND/TODO-FRONTEND/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import { RegistrationComponent } from './Components/registration/registration.component';
+import { TodoListComponent } from './Components/todo-list/todo-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'todolist', component: TodoListComponent},
+  { path: 'register', component: RegistrationComponent},
+  { path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
