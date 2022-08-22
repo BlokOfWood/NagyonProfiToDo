7714efd4923c19666fe13a1a4efb3978ab658703
BlokOfWood <strikerelite@outlook.hu>
2022-08-22 11:28:16 +0200

    base website structure

--- FRONTEND/TODO-FRONTEND/src/app/Components/todo-list/todo-list.component.ts
+++ FRONTEND/TODO-FRONTEND/src/app/Components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskPriority, TodoItem } from 'src/app/interfaces';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
+  listOfTasks: TodoItem[] = [
+    {
+      taskCreatedAt:new Date(), 
+      taskDeadline: new Date(), 
+      taskDone: true, 
+      taskID: 1, 
+      taskName: "asdasda", 
+      taskPriority: TaskPriority.CRITICAL, 
+      taskText:"as"
+    },
+    {
+      taskCreatedAt:new Date(), 
+      taskDeadline: new Date(), 
+      taskDone: false, 
+      taskID: 2, 
+      taskName: "asdasdaasdasd", 
+      taskPriority: TaskPriority.EVENTUALLY, 
+      taskText:"as"
+    }
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
