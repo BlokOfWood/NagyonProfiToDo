3658656a3d388a43f156ca70ff9cbfd9aaf4898f
BlokOfWood <strikerelite@outlook.hu>
2022-08-22 09:21:24 +0200

    base components created

--- FRONTEND/TODO-FRONTEND/src/app/app.module.ts
+++ FRONTEND/TODO-FRONTEND/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './Components/todo-list/todo-list.component';
+import { RegistrationComponent } from './Components/registration/registration.component';
+import { LoginComponent } from './Components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
