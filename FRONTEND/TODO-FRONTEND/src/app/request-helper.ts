235d061ad1aa835ba7822c84c6e62ea7586276f1
BlokOfWood <strikerelite@outlook.hu>
2022-08-24 22:56:52 +0200

    added patch method to request helper

--- FRONTEND/TODO-FRONTEND/src/app/request-helper.ts
+++ FRONTEND/TODO-FRONTEND/src/app/request-helper.ts
@@ -75,4 +75,38 @@ export class APIFunctions {
         });
         return observable;
     }
+
+    patch(endpoint: string, body: any, headers: Headers = new Headers()): Observable<string> {
+        let sessionID = localStorage.getItem('sessionID')
+
+        if (sessionID !== null && headers.get('sessionID') == null)
+            headers.append('sessionID', sessionID);
+
+        const requestAddress = this.constructAddress(endpoint)
+
+        headers.append("Content-Type", "application/json");
+
+        console.log(JSON.stringify(body))
+        var requestOptions = {
+            body: JSON.stringify(body),
+            method: 'PATCH',
+            headers: headers,
+        };
+
+        const observable = new Observable<string>(subscriber => {
+            fetch(requestAddress, requestOptions)
+                .then(response => response.text().then(x => {
+                    if (response.status === 200) {
+                        subscriber.next(x)
+                        subscriber.complete();
+                    }
+                    else {
+                        subscriber.error(new Error(`Failed Request. Code: ${response.status}`))
+                    }
+                }))
+                .then(result => console.log(result))
+                .catch(error => console.log(subscriber.error(new Error(`Failed Request. Error: ${error}`))))
+        });
+        return observable;
+    }
 }
