e08adf5191c4119308ec73a781f89e8e4a73a730
BlokOfWood <strikerelite@outlook.hu>
2022-09-30 01:48:25 +0200

    Auth Guard that redirects back to login page if the user is not logged in

--- FRONTEND/TODO-FRONTEND/src/app/app-routing.module.ts
+++ FRONTEND/TODO-FRONTEND/src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Components/login/login.component';
 import { RegistrationComponent } from './Components/registration/registration.component';
 import { TodoListComponent } from './Components/todo-list/todo-list.component';
+import {AuthGuardService} from "./auth-guard.service";
 
 const routes: Routes = [
-  { path: 'todolist', component: TodoListComponent},
+  { path: 'todolist', component: TodoListComponent, canActivate: [AuthGuardService]},
   { path: 'register', component: RegistrationComponent},
   { path: 'login', component: LoginComponent}
 ];
